61553383f14f001fd953786f60aa68413d23c1da
Akira Miyakoda <akira.miyakoda@gmail.com>
2020-11-05 09:19:49 +0900

    Fonts: Add a script to download and install fonts instead of having them in the repo.

---
+++ install-fonts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SARASA_VER='0.14.5'
+SARASA_FILE="sarasa-gothic-ttf-$SARASA_VER"
+SARASA_URL="https://github.com/be5invis/Sarasa-Gothic/releases/download/v$SARASA_VER/$SARASA_FILE.7z"
+cd /tmp
+wget "$SARASA_URL"
+7z x "$SARASA_FILE.7z"
+
+mkdir -p ~/.fonts
+mv sarasa-*-j-*.ttf ~/.fonts
+rm -rf sarasa-*
