9cdfbce98ca747140fbdbc292b8c96555ae57543
Akira Miyakoda <akira.miyakoda@gmail.com>
2021-06-25 11:53:56 +0900

    Always require sudo when installing fonts.

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ $(id -u) -ne 0 ]; then
+    echo "Sudo requred."
+    exit 1
+fi
+
 SARASA_VER='0.32.6'
 SARASA_FILE="sarasa-gothic-ttf-$SARASA_VER"
 SARASA_URL="https://github.com/be5invis/Sarasa-Gothic/releases/download/v$SARASA_VER/$SARASA_FILE.7z"
@@ -7,11 +12,7 @@ cd /tmp
 wget "$SARASA_URL"
 7z x "$SARASA_FILE.7z"
 
-if [ $(id -u) -eq 0 ]; then
-    DESTIN_DIR='/usr/share/fonts/truetype/sarasa-gothic/'
-else
-    DESTIN_DIR='~/.fonts'
-fi
+DESTIN_DIR='/usr/share/fonts/truetype/sarasa-gothic/'
 
 mkdir -p "$DESTIN_DIR"
 mv sarasa-*-j-*.ttf "$DESTIN_DIR"
