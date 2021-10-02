9060fdde5681b9918a3323dd4329e93102a60782
Akira Miyakoda <akira.miyakoda@gmail.com>
2021-10-03 06:30:42 +0900

    Bump Sarasa Gothic to v0.34.4.

--- install-fonts.sh
+++ install-fonts.sh
@@ -11,7 +11,7 @@ apt install -y p7zip-full fonts-mplus fonts-cascadia-code
 
 # Install Sarasa Gothic
 
-SARASA_VER='0.34.3'
+SARASA_VER='0.34.4'
 SARASA_FILE="sarasa-gothic-ttf-${SARASA_VER}.7z"
 SARASA_URL="https://github.com/be5invis/Sarasa-Gothic/releases/download/v${SARASA_VER}/${SARASA_FILE}"
 
